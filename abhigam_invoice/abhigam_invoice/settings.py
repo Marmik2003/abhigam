7b8c67b9015d7115767accc2d641047b19171195
Dularkumar <sharmadular7392@gmail.com>
2020-11-01 12:29:14 +0530

    models.py file added

--- abhigam_invoice/abhigam_invoice/settings.py
+++ abhigam_invoice/abhigam_invoice/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'abhigam_invoice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'abhigam',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5433'
     }
 }
 
